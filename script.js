29c56522672272f687dd1e58dbd0df76577d4b10
OXsatoshi <afia.nabil24@gmail.com>
2024-03-30 04:26:41 +0100

    Add random color generation
    -Make the color change each time the user hover over a div

--- script.js
+++ script.js
@@ -12,15 +12,23 @@ function handleClick(){
     removeAllChildren(container);
     makeTheGridLayour(size);
 }
+function generateRandomRGB() {
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+
+    return [red, green, blue];
+}
 submitBtn.addEventListener("click",handleClick);
 function handlHoverOverDiv(event){
-    console.log(event.target);
-    event.target.style.backgroundColor = "red";
+    var colorArray = generateRandomRGB();
+    var colorString = 'rgb(' + colorArray[0] + ', ' + colorArray[1] + ', ' + colorArray[2] + ')';
+    event.target.style.backgroundColor = colorString;
     let opacity = parseFloat(event.target.style.opacity);
     opacity= opacity+0.1;
 
     event.target.style.opacity = opacity;
-    console.log(opacity);
+
 }
 function makeTheGridLayour(numberOfboxes){
     for(let i = 0;i<numberOfboxes;i++){
