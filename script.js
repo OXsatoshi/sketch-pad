0267e34fea8abf293203632c6fd9f79878b4ae5b
OXsatoshi <afia.nabil24@gmail.com>
2024-03-30 01:30:58 +0100

    Add event listner to all divs of the grid
    -Listen to the event mousehover when the event occur on a div
     the handler change the background color of that div

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const numberOfboxes = 16;
 const container = document.querySelector(".container");
-
-for(let i = 0;i<numberOfboxes;i++){
-    let divRow = document.createElement("div");
-    divRow.style.display="flex";
-    divRow.style.flex = 1;
-    for(let j=0;j<numberOfboxes;j++){
-        let div = document.createElement("div");
-        divRow.appendChild(div);
-        div.style.border="1px solid black";
-        div.style.flex = 1;
-        container.appendChild(divRow);
+function handlHoverOverDiv(event){
+    console.log(event.target);
+    event.target.style.backgroundColor = "red";
+}
+function makeTheGridLayour(numberOfboxes){
+    for(let i = 0;i<numberOfboxes;i++){
+        let divRow = document.createElement("div");
+        divRow.style.display="flex";
+        divRow.style.flex = 1;
+        for(let j=0;j<numberOfboxes;j++){
+            let div = document.createElement("div");
+            divRow.appendChild(div);
+            div.style.border="1px solid black";
+            div.style.flex = 1;
+            div.addEventListener("mouseover",handlHoverOverDiv);
+            container.appendChild(divRow);
+        }
     }
 }
+
+makeTheGridLayour(numberOfboxes);
