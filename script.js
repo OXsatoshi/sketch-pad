0b8d58395a87e75e9e5a0d1d67bb5e04f01e8769
OXsatoshi <afia.nabil24@gmail.com>
2024-03-30 04:16:02 +0100

    Add the resize of the grid
    -Give the user the ability to change default size of the grid(16*16)

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const numberOfboxes = 16;
 const container = document.querySelector(".container");
+const submitBtn = document.querySelector("button");
+const input = document.querySelector("input");
+function removeAllChildren(parentNode) {
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+}
+function handleClick(){
+    let size = input.value;
+    removeAllChildren(container);
+    makeTheGridLayour(size);
+}
+submitBtn.addEventListener("click",handleClick);
 function handlHoverOverDiv(event){
     console.log(event.target);
     event.target.style.backgroundColor = "red";
+    let opacity = parseFloat(event.target.style.opacity);
+    opacity= opacity+0.1;
+
+    event.target.style.opacity = opacity;
+    console.log(opacity);
 }
 function makeTheGridLayour(numberOfboxes){
     for(let i = 0;i<numberOfboxes;i++){
@@ -14,9 +32,11 @@ function makeTheGridLayour(numberOfboxes){
             divRow.appendChild(div);
             div.style.border="1px solid black";
             div.style.flex = 1;
+            div.style.opacity = 0;
             div.addEventListener("mouseover",handlHoverOverDiv);
-            container.appendChild(divRow);
+            
         }
+        container.appendChild(divRow);
     }
 }
 
